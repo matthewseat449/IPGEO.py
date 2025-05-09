f7782743a5251b4121e69213fc685bab8d320434
matthewseat449 <199787187+matthewseat449@users.noreply.github.com>
2025-05-09 13:08:57 -0400

    Update ipgeo.py

--- ipgeo.py
+++ ipgeo.py
@@ -1,19 +1,27 @@
 #quick little IP geolocation script I threw together. Gives your public IP, region of world/country, country, city, and org.
 #usage is just 'python ipgeo.py' and you are all set, assuming you have the imported modules.
 
-import re
+#edited usage for python3 - functions are the exact same,but using python3 instead of 2
 import json
-from urllib2 import urlopen
+from urllib.request import urlopen
 
-url = 'http://ipinfo.io/json'
-response = urlopen(url)
-data = json.load(response)
+def get_ip_info():
+    url = 'http://ipinfo.io/json'
+    with urlopen(url) as response:
+        data = json.load(response)
 
-IP=data['ip']
-org=data['org']
-city = data['city']
-country=data['country']
-region=data['region']
+    ip = data.get('ip', 'N/A')
+    org = data.get('org', 'N/A')
+    city = data.get('city', 'N/A')
+    country = data.get('country', 'N/A')
+    region = data.get('region', 'N/A')
 
-print 'Your IP detail\n '
-print 'IP : {4} \nRegion : {1} \nCountry : {2} \nCity : {3} \nOrg : {0}'.format(org,region,country,city,IP)
+    print('Your IP detail\n')
+    print('IP      : {}'.format(ip))
+    print('Region  : {}'.format(region))
+    print('Country : {}'.format(country))
+    print('City    : {}'.format(city))
+    print('Org     : {}'.format(org))
+
+if __name__ == '__main__':
+    get_ip_info()
